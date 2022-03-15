839318c1abd47f4b794bcaf91e1d33249e2eea10
chaldrich24 <chaldrich24@gmail.com>
2022-03-15 18:32:46 -0500

    initial file structure and beginning components

---
+++ constants/colors.js
@@ -0,0 +1,4 @@
+export default {
+    primary: '#3C91E6',
+    accent: '#342E37'
+};
---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,24 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Platform } from 'react-native';
+
+import colors from '../constants/colors';
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+
+const ProductsNavigator = createStackNavigator(
+    {
+        ProductsOverview: ProductsOverviewScreen, 
+        ProductDetail: ProductDetailScreen
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: Platform.OS === 'android' ? colors.primary : ''
+            },
+            headerTintColor: Platform.OS === 'android' ? 'white' : colors.primary
+        }
+    }
+);
+
+export default createAppContainer(ProductsNavigator);
